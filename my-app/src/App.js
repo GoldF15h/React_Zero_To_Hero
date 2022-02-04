1c9d10ab6fae96276ac1477cd4b48b318077bb09
GoldF15h <todsapon.singsunjit@gmail.com>
2022-02-05 01:11:57 +0700

    first half

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="LEARN"></Todo>
+      <Todo text="MASTER"></Todo>
+      <Todo text="TEACH"></Todo>
+    </div>
+  );
+}
+
+export default App;
